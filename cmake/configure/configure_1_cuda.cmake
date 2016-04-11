379a9efebedc3da7138dd285992f7f3c1470c69e
Matthias Maier <tamiko@43-1.org>
2016-04-11 18:45:26 -0500

    CMake: Guard CUDA_WRAP_SRCS invocation and rearrange

--- cmake/configure/configure_1_cuda.cmake
+++ cmake/configure/configure_1_cuda.cmake
@@ -1,6 +1,6 @@
 ## ---------------------------------------------------------------------
 ##
-## Copyright (C) 2012 - 2015 by the deal.II authors
+## Copyright (C) 2012 - 2016 by the deal.II authors
 ##
 ## This file is part of the deal.II library.
 ##
@@ -22,8 +22,8 @@ MACRO(FEATURE_CUDA_FIND_EXTERNAL var)
 
   IF(CUDA_FOUND)
     SET(${var} TRUE)
-
   ENDIF()
+
 ENDMACRO()
 
 MACRO(FEATURE_CUDA_ERROR_MESSAGE)
