babff877ebc642e9c4a43856ae7f6d71da52fbf5
Karl Ljungkvist <k.ljungkvist@gmail.com>
2016-04-19 15:46:50 -0500

    only conditionally enable the cuda code when DEAL_II_WITH_CUDA is ON

--- include/deal.II/lac/gpu_vector.h
+++ include/deal.II/lac/gpu_vector.h
@@ -1,7 +1,26 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
 #ifndef dealii__gpu_vector_h
 #define dealii__gpu_vector_h
 
 #include <deal.II/base/config.h>
+
+#ifdef DEAL_II_WITH_CUDA
+
+// includes...
 
 DEAL_II_NAMESPACE_OPEN
 
@@ -19,4 +38,6 @@ public:
 
 DEAL_II_NAMESPACE_CLOSE
 
-#endif
+#endif    // DEAL_II_WITH_CUDA
+
+#endif    // dealii__gpu_vector_h
--- source/lac/gpu_vector.cu
+++ source/lac/gpu_vector.cu
@@ -1,4 +1,22 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (C) 2016 by the deal.II authors
+//
+// This file is part of the deal.II library.
+//
+// The deal.II library is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the deal.II distribution.
+//
+// ---------------------------------------------------------------------
+
 #include <deal.II/lac/gpu_vector.h>
+
+#ifdef DEAL_II_WITH_CUDA
+
 #include <cstdio>
 
 DEAL_II_NAMESPACE_OPEN
@@ -37,3 +55,5 @@ void GpuVector::hello()
 }
 
 DEAL_II_NAMESPACE_CLOSE
+
+#endif
