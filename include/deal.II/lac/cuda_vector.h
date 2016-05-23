c89dc5c96e5d35f58487b9a84beee6a5c38a0ae2
Bruno Turcksin <bruno.turcksin@gmail.com>
2016-05-23 13:27:20 -0400

    Add more functions to cuda vector.

--- include/deal.II/lac/cuda_vector.h
+++ include/deal.II/lac/cuda_vector.h
@@ -19,218 +19,226 @@
 #include <deal.II/base/config.h>
 #include <deal.II/lac/vector_space_vector.h>
 
-// Using DEAL_II_NAMESPACE_OPEN creates a strange error:
-// "This declaration has no storage class or type specifier"
-namespace dealii
+
+#ifdef DEAL_II_WITH_CUDA
+
+DEAL_II_NAMESPACE_OPEN
+
+class CommunicationPatternBase;
+class IndexSet;
+template <typename Number> class ReadWriteVector;
+
+namespace LinearAlgebra
 {
-
-  class CommunicationPatternBase;
-  class IndexSet;
-  template <typename Number> class ReadWriteVector;
-
-  namespace LinearAlgebra
+  namespace CUDAWrappers
   {
-    namespace CUDAWrappers
+    /**
+     * This class implement vector using CUDA for use on Nvidia GPUs. This
+     * class is derived from the LinearAlgebra::VectorSpaceVector class.
+     *
+     * @ingroup CUDAWrappers
+     * @ingroup Vectors
+     * @author Karl Ljungkvist, Bruno Turcksin, 2016
+     */
+    template <typename Number>
+    class Vector: public VectorSpaceVector<Number>
     {
+    public:
+      typedef typename VectorSpaceVector<Number>::size_type size_type;
+      typedef typename VectorSpaceVector<Number>::real_type real_type;
+
       /**
-       * This class implement vector using CUDA for use on Nvidia GPUs. This
-       * class is derived from the LinearAlgebra::VectorSpaceVector class.
-       *
-       * @ingroup CUDAWrappers
-       * @ingroup Vectors
-       * @author Karl Ljungkvist, Bruno Turcksin, 2016
+       * Constructor. Create a vector of dimension zero.
        */
-      template <typename Number>
-      class Vector: public VectorSpaceVector<Number>
-      {
-      public:
-        typedef typename VectorSpaceVector<Number>::size_type size_type;
-        typedef typename VectorSpaceVector<Number>::real_type real_type;
+      Vector();
 
-        /**
-         * Constructor. Create a vector of dimension zero.
-         */
-        Vector();
+      /**
+       * Copy constructor.
+       */
+      Vector(const Vector<Number> &V);
 
-        /**
-         * Copy constructor.
-         */
-        Vector(const Vector<Number> &V);
+      /**
+       * Constructor. Set dimension to @p n and initialize all elements with
+       * zero.
+       *
+       * The constructor is made explicit to avoid accident like this:
+       * <tt>v=0;</tt>. Presumably, the user wants to set every elements of
+       * the vector to zero, but instead, what happens is this call:
+       * <tt>v=Vector@<Number@>(0);</tt>, i.e. the vector is replaced by one
+       * of length zero.
+       */
+      explicit Vector(const size_type n);
 
-        /**
-         * Constructor. Set dimension to @p n and initialize all elements with
-         * zero.
-         *
-         * The constructor is made explicit to avoid accident like this:
-         * <tt>v=0;</tt>. Presumably, the user wants to set every elements of
-         * the vector to zero, but instead, what happens is this call:
-         * <tt>v=Vector@<Number@>(0);</tt>, i.e. the vector is replaced by one
-         * of length zero.
-         */
-        explicit Vector(const size_type n);
+      /**
+       * Destructor.
+       */
+      ~Vector();
 
-        /**
-         * Destructor.
-         */
-        ~Vector();
+      /**
+       * Reinit functionality. The flag <tt>omit_zeroing_entries</tt>
+       * determines wheter the vector should be filled with zero (false) or
+       * left untouched (true).
+       */
+      void reinit(const size_type n,
+                  const bool      omit_zeroing_entries = false);
 
-        /**
-         * Reinit functionality. The flag <tt>omit_zeroing_entries</tt>
-         * determines wheter the vector should be filled with zero (false) or
-         * left untouched (true).
-         */
-        void reinit(const size_type n,
-                    const bool      omit_zeroing_entries = false);
+      /**
+       * Import all the element from the input vector @p V.
+       * VectorOperation::values @p operation is used to decide if the
+       * elements int @p V should be added to the current vector or replace
+       * the current elements. The last parameter is not used. It is only used
+       * for distributed vectors. This is the function that should be used to
+       * copy a vector to the GPU.
+       */
+      virtual void import(const ReadWriteVector<Number> &V,
+                          VectorOperation::values operation,
+                          std_cxx11::shared_ptr<const CommunicationPatternBase>);
 
-        /**
-         * Import all the element from the input vector @p V.
-         * VectorOperation::values @p operation is used to decide if the
-         * elements int @p V should be added to the current vector or replace
-         * the current elements. The last parameter is not used. It is only used
-         * for distributed vectors. This is the function that should be used to
-         * copy a vector to the GPU.
-         */
-        virtual void import(const ReadWriteVector<Number> &V,
-                            VectorOperation::values operation,
-                            std_cxx11::shared_ptr<const CommunicationPatternBase>);
+      /**
+       * Multiply the entive vector by a fixed factor.
+       */
+      virtual Vector<Number> &operator*= (const Number factor);
 
-        /**
-         * Multiply the entive vector by a fixed factor.
-         */
-        virtual Vector<Number> &operator*= (const Number factor);
+      /**
+       * Divide the entire vector by a fixed factor.
+       */
+      virtual Vector<Number> &operator/= (const Number factor);
 
-        /**
-         * Divide the entire vector by a fixed factor.
-         */
-        virtual Vector<Number> &operator/= (const Number factor);
+      /**
+       * Add the vector @p V to the present one.
+       */
+      virtual Vector<Number> &operator+= (const VectorSpaceVector<Number> &V);
 
-        /**
-         * Add the vector @p V to the present one.
-         */
-        virtual Vector<Number> &operator+= (const VectorSpaceVector<Number> &V);
+      /**
+       * Subtract the vector @p V from the present one.
+       */
+      virtual Vector<Number> &operator-= (const VectorSpaceVector<Number> &V);
 
-        /**
-         * Subtract the vector @p V from the present one.
-         */
-        virtual Vector<Number> &operator-= (const VectorSpaceVector<Number> &V);
+      /**
+       * Return the scalar product of two vectors.
+       */
+      virtual Number operator* (const VectorSpaceVector<Number> &V);
 
-        /**
-         * Return the scalar product of two vectors.
-         */
-        virtual Number operator* (const VectorSpaceVector<Number> &V);
+      /**
+       * Add @p to all components. Not that @p a is a scalar not a vector.
+       */
+      virtual void add(const Number a);
 
-        /**
-         * Add @p to all components. Not that @p a is a scalar not a vector.
-         */
-        virtual void add(const Number a);
+      /**
+       * Simple addition of a multiple of a vector, i.e. <tt>*thos += a*V</tt>.
+       */
+      virtual void add(const Number a, const VectorSpaceVector<Number> &V);
 
-        /**
-         * Simple addition of a multiple of a vector, i.e. <tt>*thos += a*V</tt>.
-         */
-        virtual void add(const Number a, const VectorSpaceVector<Number> &V);
+      /**
+       * Multiple addition of scaled vectors, i.e. <tt>*this += a*V</tt>.
+       */
+      virtual void add(const Number a, const VectorSpaceVector<Number> &V,
+                       const Number b, const VectorSpaceVector<Number> &W);
 
-        /**
-         * Multiple addition of scaled vectors, i.e. <tt>*this += a*V</tt>.
-         */
-        virtual void add(const Number a, const VectorSpaceVector<Number> &V,
-                         const Number b, const VectorSpaceVector<Number> &W);
+      /**
+       * Scaling and simple addition of a multiple of a vector, i.e. <tt>*this
+       * = s*(*this)+a*V</tt>
+       */
+      virtual void sadd(const Number s, const Number a,
+                        const VectorSpaceVector<Number> &V);
 
-        /**
-         * Scaling and simple addition of a multiple of a vector, i.e. <tt>*this
-         * = s*(*this)+a*V</tt>
-         */
-        virtual void sadd(const Number s, const Number a,
-                          const VectorSpaceVector<Number> &V);
+      /**
+       * Scale each element of this vector by the corresponding element in the
+       * argument. This function is mostly meant to simulation multiplication
+       * (and immediate re-assignment) by a diagonal scaling matrix.
+       */
+      virtual void scale(const VectorSpaceVector<Number> &scaling_factors);
 
-        /**
-         * Scale each element of this vector by the corresponding element in the
-         * argument. This function is mostly meant to simulation multiplication
-         * (and immediate re-assignment) by a diagonal scaling matrix.
-         */
-        virtual void scale(const VectorSpaceVector<Number> &scaling_factors);
+      /**
+       * Assignement <tt>*this = a*V</tt>.
+       */
+      virtual void equ(const Number a, const VectorSpaceVector<Number> &V);
 
-        /**
-         * Assignement <tt>*this = a*V</tt>.
-         */
-        virtual void equ(const Number a, const VectorSpaceVector<Number> &V);
+      /**
+       * Return the l<sub>1</sub> norm of the vector (i.e., the sum of the
+       * absolute values of all entries among all processors).
+       */
+      virtual real_type l1_norm() const;
 
-        /**
-         * Return the l<sub>1</sub> norm of the vector (i.e., the sum of the
-         * absolute values of all entries among all processors).
-         */
-        virtual real_type l1_norm() const;
+      /**
+       * Return the l<sub>2</sub> norm of the vector (i.e., the square root of
+       * the sum of the square of all entries among all processors).
+       */
+      virtual real_type l2_norm() const;
 
-        /**
-         * Return the l<sub>2</sub> norm of the vector (i.e., the square root of
-         * the sum of the square of all entries among all processors).
-         */
-        virtual real_type l2_norm() const;
+      /**
+       * Return the maximum norm of the vector (i.e., the maximum absolute
+       * value among all entries and among all processors).
+       */
+      virtual real_type linfty_norm() const;
 
-        /**
-         * Return the maximum norm of the vector (i.e., the maximum absolute
-         * value among all entries and among all processors).
-         */
-        virtual real_type linfty_norm() const;
+      /**
+       * Perform a combined operation of a vector addition and a subsequent
+       * inner product, returning the value of the inner product. In other
+       * words, the result of this function is the same as if the user called
+       * @code
+       * this->add(a, V);
+       * return_value = *this * W;
+       * @endcode
+       *
+       * The reason this function exists is that this operation involves less
+       * memory transfer than calling the two functions separately. This
+       * method only needs to load three vectors, @p this, @p V, @p W, whereas
+       * calling separate methods means to load the calling vector @p this
+       * twice. Since most vector operations are memory transfer limited, this
+       * reduces the time by 25\% (or 50\% if @p W equals @p this).
+       */
+      virtual Number add_and_dot(const Number a,
+                                 const VectorSpaceVector<Number> &V,
+                                 const VectorSpaceVector<Number> &W);
 
-        /**
-         * Perform a combined operation of a vector addition and a subsequent
-         * inner product, returning the value of the inner product. In other
-         * words, the result of this function is the same as if the user called
-         * @code
-         * this->add(a, V);
-         * return_value = *this * W;
-         * @endcode
-         *
-         * The reason this function exists is that this operation involves less
-         * memory transfer than calling the two functions separately. This
-         * method only needs to load three vectors, @p this, @p V, @p W, whereas
-         * calling separate methods means to load the calling vector @p this
-         * twice. Since most vector operations are memory transfer limited, this
-         * reduces the time by 25\% (or 50\% if @p W equals @p this).
-         */
-        virtual Number add_and_dot(const Number a,
-                                   const VectorSpaceVector<Number> &V,
-                                   const VectorSpaceVector<Number> &W);
+      /**
+       * Return the size of the vector.
+       */
+      virtual size_type size();
 
-        /**
-         * Return the size of the vector.
-         */
-        virtual size_type size();
+      /**
+       * Return an index set that describe which elements of this vector are
+       * owned by the current processor, i.e. [0, size).
+       */
+      virtual dealii::IndexSet locally_owned_elements();
 
-        /**
-         * Return an index set that describe which elements of this vector are
-         * owned by the current processor, i.e. [0, size).
-         */
-        virtual dealii::IndexSet locally_owned_elements();
+      /**
+       * Print the vector to the output stream @p out.
+       */
+      virtual void print(std::ostream &out,
+                         const unsigned int precision=2,
+                         const bool scientific=true,
+                         const bool across=true) const;
 
-        /**
-         * Print the vector to the output stream @p out.
-         */
-        virtual void print(std::ostream &out,
-                           const unsigned int precision=2,
-                           const bool scientific=true,
-                           const bool across=true) const;
+      /**
+       * Return the memory consumption of this class in bytes.
+       */
+      virtual std::size_t memory_consumption() const;
 
-        /**
-         * Return the memory consumption of this class in bytes.
-         */
-        virtual std::size_t memory_consumption() const;
+      /**
+       * Attempt to perform an operation between two incompatible vector types.
+       *
+       * @ingroup Exceptions
+       */
+      DeclException0(ExcVectorTypeNotCompatible);
 
-      private:
-        /**
-         * Pointer to the array of elements of this vector.
-         */
-        Number *val;
+    private:
+      /**
+       * Pointer to the array of elements of this vector.
+       */
+      Number *val;
 
-        /**
-         * Number of elements in the vector.
-         */
-        size_type n_elements;
-      };
-    }
+      /**
+       * Number of elements in the vector.
+       */
+      size_type n_elements;
+    };
   }
-
 }
-//DEAL_II_NAME_CLOSE
+
+DEAL_II_NAMESPACE_CLOSE
+
+#endif
 
 #endif
--- source/lac/cuda_vector.cu
+++ source/lac/cuda_vector.cu
@@ -20,10 +20,183 @@
 
 DEAL_II_NAMESPACE_OPEN
 
+#define BKSIZE_ELEMWISE_OP 512
+#define CHUNKSIZE_ELEMWISE_OP 8
+#define VR_BKSIZE 512
+#define VR_CHUNK_SIZE 8
+      
 namespace LinearAlgebra
 {
   namespace CUDAWrappers
   {
+    namespace internal
+    {
+      template <typename Number>
+      __global__ void vec_scale(Number *val, const Number a, const unsigned int N)
+      {
+        const unsigned int idx_base = threadIdx.x + blockIdx.x *
+          (blockDim.x*CHUNKSIZE_ELEMWISE_OP);
+        for (unsigned int i=0; i<CHUNKSIZE_ELEMWISE_OP; ++i)
+        {
+          const unsigned int idx = idx_base + i*BKSIZE_ELEMWISE_OP;
+          if (idx<N)
+            val[idx] = a;
+        }
+      }
+
+
+
+      struct Binop_Addition
+      {
+        template <typename Number>
+        __device__ static inline Number operation(const Number a, 
+            const Number b)
+        {
+          return a+b;
+        }
+      };
+
+
+
+      struct Binop_Subtraction
+      {
+        template <typename Number>
+        __device__ static inline Number operation(const Number a,
+            const Number b)
+        {
+          return a-b;
+        }
+      };
+
+
+
+      template <typename Number, typename Binop>
+      __global__ void vector_bin_op(Number* v1, Number* v2, const int N)
+      {
+        const unsigned int idx_base = threadIdx.x + blockIdx.x *
+          (blockDim.x*CHUNKSIZE_ELEMWISE_OP);
+        for (unsigned int i=0; i<CHUNKSIZE_ELEMWISE_OP; ++i)
+        {
+          const unsigned int idx = idx_base + i*BKSIZE_ELEMWISE_OP;
+          if (idx<N)
+            v1[idx] = Binop::operation(v1[idx],v2[idx]);
+        }
+      }
+
+
+
+      template <typename Number, typename Operation>
+      __device__ void reduce_within_warp(volatile Number* result_buffer,
+          unsigned int local_idx)
+      {
+        if (VR_BKSIZE >= 64)
+          result_buffer[local_idx] = 
+            Operation::reduction_op(result_buffer[local_idx],
+                result_buffer[local_idx+32]);
+        if (VR_BKSIZE >= 32)
+          result_buffer[local_idx] = 
+            Operation::reduction_op(result_buffer[local_idx],
+                result_buffer[local_idx+16]);
+        if (VR_BKSIZE >= 16)
+          result_buffer[local_idx] = 
+            Operation::reduction_op(result_buffer[local_idx],
+                result_buffer[local_idx+8]);
+        if (VR_BKSIZE >= 8)
+          result_buffer[local_idx] = 
+            Operation::reduction_op(result_buffer[local_idx],
+                result_buffer[local_idx+4]);
+        if (VR_BKSIZE >= 4)
+          result_buffer[local_idx] = 
+            Operation::reduction_op(result_buffer[local_idx],
+                result_buffer[local_idx+2]);
+        if (VR_BKSIZE >= 2)
+          result_buffer[local_idx] = 
+            Operation::reduction_op(result_buffer[local_idx],
+                result_buffer[local_idx+1]);
+      }
+
+
+
+      template <typename Number, typename Operation>
+      __device__ void reduce(Number* result, Number* result_buffer,
+          const unsigned int local_idx, const unsigned int global_idx, 
+          const unsigned int N)
+      {
+        // TODO why 32?
+        for (unsigned int s=VR_BKSIZE/2; s>32; s=s>>1)
+        {
+          if (local_idx < s)
+            result_buffer[local_idx] = Operation::reduction_op(result_buffer[local_idx],
+                  result_buffer[local_idx+s]);
+          __syncthreads();
+        }
+
+        if (local_idx < 32)
+          reduce_within_warp<Number,Operation>(result_buffer, local_idx);
+
+        if (local_idx == 0)
+          Operation::atomic_op(result, result_buffer[0]);
+      }
+
+
+
+      template <typename Number>
+      struct DotProduct
+      {
+        __device__ static Number binary_op(const Number a, const Number b) 
+        {
+          return a*b;
+        }
+
+        __device__ static Number reduction_op(const Number a, const Number b)
+        {
+          return a+b;
+        }
+
+        __device__ static Number atomic_op(const Number a, Number* dst)
+        {
+          return atomicAdd(dst, a);
+        }
+
+        __device__ static Number null_value()
+        {
+          return Number();
+        }
+      };
+
+
+
+      template <typename Number, typename Operation>
+      __global__ void double_vector_reduction(Number *result, const Number* v1,
+          const Number v2, const int N)
+      {
+        __shared__ Number result_buffer[VR_BKSIZE];
+
+        const unsigned int global_idx = threadIdx.x + blockIdx.x*(blockDim.x*VR_CHUNK_SIZE);
+        const unsigned int local_idx = threadIdx.x;
+
+        if (global_idx<N)
+          result_buffer[local_idx] = Operation::binary_op(v1[global_idx],v2[global_idx]);
+        else
+          result_buffer[local_idx] = Operation::null_value();
+
+        for (unsigned int i=1; i<VR_CHUNK_SIZE; ++i)
+        {
+          const unsigned int idx = global_idx + i*VR_BKSIZE;
+          if (idx<N)
+            result_buffer[local_idx] =
+              Operation::reduction_op(result_buffer[local_idx],
+                  Operation::binary_op(v1[idx], v2[idx]));
+        }
+
+        __syncthreads();
+
+        reduce<Number,Operation> (result,result_buffer,local_idx,global_idx,N);
+      }
+    }
+
+
+
     template <typename Number>
     Vector<Number>::Vector()
     :
@@ -50,7 +223,7 @@ namespace LinearAlgebra
 
 
     template <typename Number>
-    Vector(const size_type n)
+    Vector<Number>::Vector(const size_type n)
     :
       n_elements(n)
     {
@@ -76,8 +249,8 @@ namespace LinearAlgebra
 
 
     template <typename Number>
-    Vector<Number>::reinit(const size_type n,
-                           const bool      omit_zeroing_entries)
+    void Vector<Number>::reinit(const size_type n,
+                                const bool      omit_zeroing_entries)
     {
       // Resize the underlying array if necessary
       if (n == 0)
@@ -91,7 +264,7 @@ namespace LinearAlgebra
       }
       else
       {
-        if (n_element != n)
+        if (n_elements != n)
         {
           cudaError_t error_code = cudaFree(val);
           CudaAssert(error_code);
@@ -109,6 +282,147 @@ namespace LinearAlgebra
             n_elements*sizeof(Number));
         CudaAssert(error_code);
       }
+    }
+
+
+
+    template <typename Number>
+    void Vector<Number>::import(const ReadWriteVector<Number> &V,
+        VectorOperation::values operation,
+        std_cxx11::shared_ptr<const CommunicationPatternBase> )
+    {
+      //TODO
+    }
+
+
+
+    template <typename Number>
+    Vector<Number>& Vector<Number>::operator*= (const Number factor)
+    {
+      AssertIsFinite(factor);
+      const int n_blocks = 1 +
+        (n_elements-1)/(CHUNKSIZE_ELEMWISE_OP*BKSIZE_ELEMWISE_OP);
+      internal::vec_scale<Number> <<<n_blocks,BKSIZE_ELEMWISE_OP>>>(val,
+          factor, n_elements);
+
+      // Check that the kernel was launched correctly
+      CudaAssert(cudaGetLastError());
+      // Check that there was no problem during the execution of the kernel
+      CudaAssert(cudaDeviceSynchronize());
+
+      return *this;
+    }
+
+
+
+    template <typename Number>
+    Vector<Number>& Vector<Number>::operator/= (const Number factor)
+    {
+      AssertIsFinite(factor);
+      Assert(factor!=Number(0.), ExcZero());
+      const int n_blocks = 1 +
+        (n_elements-1)/(CHUNKSIZE_ELEMWISE_OP*BKSIZE_ELEMWISE_OP);
+      internal::vec_scale<Number> <<<n_blocks,BKSIZE_ELEMWISE_OP>>>(val,
+          1./factor, n_elements);
+
+      // Check that the kernel was launched correctly
+      CudaAssert(cudaGetLastError());
+      // Check that there was no problem during the execution of the kernel
+      CudaAssert(cudaDeviceSynchronize());
+
+      return *this;
+    }
+
+
+
+    template <typename Number>
+    Vector<Number>& Vector<Number>::operator+= (const VectorSpaceVector<Number> &V)
+    {
+      // Check that casting will work
+      Assert(dynamic_cast<const Vector<Number>*>(&V)!=nullptr,
+          ExcVectorTypeNotCompatible());
+
+      // Downcast V. If fails, throws an exception.
+      const Vector<Number> &down_V = dynamic_cast<const Vector<Number>&>(V);
+      Assert(down_V.size()==this->Size(),
+          ExcMessage("Cannot add two vectors with different numbers of elements"));
+
+      const int n_blocks = 1 +
+        (n_elements-1)/(CHUNKSIZE_ELEMWISE_OP*BKSIZE_ELEMWISE_OP);
+
+      internal::vector_bin_op<Number,internal::Binop_Addition>
+        <<<n_blocks,BKSIZE_ELEMWISE_OP>>>(val, down_V.val, n_elements);
+
+      // Check that the kernel was launched correctly
+      CudaAssert(cudaGetLastError());
+      // Check that there was no problem during the execution of the kernel
+      CudaAssert(cudaDeviceSynchronize());
+
+      return *this;
+    }
+
+
+
+    template <typename Number>
+    Vector<Number>& Vector<Number>::operator-= (const VectorSpaceVector<Number> &V)
+    {
+      // Check that casting will work
+      Assert(dynamic_cast<const Vector<Number>*>(&V)!=nullptr,
+          ExcVectorTypeNotCompatible());
+
+      // Downcast V. If fails, throws an exception.
+      const Vector<Number> &down_V = dynamic_cast<const Vector<Number>&>(V);
+      Assert(down_V.size()==this->Size(),
+          ExcMessage("Cannot add two vectors with different numbers of elements"));
+
+      const int n_blocks = 1 +
+        (n_elements-1)/(CHUNKSIZE_ELEMWISE_OP*BKSIZE_ELEMWISE_OP);
+
+      internal::vector_bin_op<Number,internal::Binop_Subtraction>
+        <<<n_blocks,BKSIZE_ELEMWISE_OP>>>(val, down_V.val, n_elements);
+
+      // Check that the kernel was launched correctly
+      CudaAssert(cudaGetLastError());
+      // Check that there was no problem during the execution of the kernel
+      CudaAssert(cudaDeviceSynchronize());
+
+      return *this;
+    }
+
+
+
+    template <typename Number>
+    Number Vector<Number>::operator* (const VectorSpaceVector<Number> &V)
+    {
+      // Check that casting will work
+      Assert(dynamic_cast<const Vector<Number>*>(&V)!=nullptr,
+          ExcVectorTypeNotCompatible());
+
+      // Downcast V. If fails, throws an exception.
+      const Vector<Number> &down_V = dynamic_cast<const Vector<Number>&>(V);
+      Assert(down_V.size()==this->Size(),
+          ExcMessage("Cannot add two vectors with different numbers of elements"));
+
+      Number* result_device;
+      cudaError_t error_code = cudaMalloc(&result_device, n_elements*sizeof(Number));
+      CudaAssert(error_code);
+      error_code = cudaMemset(result_device, Number(), sizeof(Number));
+
+      const int n_blocks = 1 + (n_elements-1)/(VR_CHUNK_SIZE*VR_BKSIZE);
+      internal::double_vector_reduction<internal::DotProduct<Number>>
+        <<<dim3(n_blocks,1),dim3(VR_BKSIZE)>>> (result_device, val,
+            down_V.val, n_elements);
+
+      // Copy the result back to the host
+      Number result;
+      error_code = cudaMemcpy(&result, result_device, sizeof(Number),
+          cudaMemcpyDeviceToHost);
+      CudaAssert(error_code);
+      // Free the memory on the device
+      error_code = cudaFree(result_device);
+      CudaAssert(error_code);
+
+      return result;
     }
   }
 }
